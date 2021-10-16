f25f73f7a7ef14204f8a060e090057ad69936781
yeonju lee <yeonju.lee1005@gmail.com>
2021-10-17 00:00:04 +0900

    백앤드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,5 +18,7 @@ function login( ) {
       "content-Type": "application/json"
     },
     body: JSON.stringify(req)
-  });
+  })
+    .then((res) => res.json())
+    .then((res) => console.log);
 };
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict"
 
+const users = {
+  id: ["woorimIT", "나개발", "김팀장" ],
+  password: ["1234", "12345", "123456"]
+}
+
 const output = {
   hello: ( req, res ) => {
     res.render("home/index")
@@ -11,7 +16,22 @@ const output = {
 
 const process = {
   login: ( req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+      password = req.body.password
+      
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.password[idx] === password) {
+        return res.json({
+          success: true,
+        })
+      };
+    };
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패했습니다."
+    });
   }
 }
 
